e9b4e34b94150577a82492b49642681dee2984df
zackaad <aaden3140@gmail.com>
2023-11-08 18:05:36 +0100

    big update: added functionality to both send and receive messages. some styling. added name and avatar of user

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,15 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useState, useEffect, useRef} from 'react';
+import { View, Text, FlatList, StyleSheet, TextInput, Button, Image} from 'react-native';
 import app from '../config/config';
-import { collection, addDoc, Firestore, serverTimestamp, getFirestore } from 'firebase/firestore'; 
+import { collection, addDoc, Firestore, serverTimestamp, getFirestore, orderBy, query, onSnapshot } from 'firebase/firestore'; 
 import db from '../config/config';
+import 'firebase/auth';
+import { useRoute } from '@react-navigation/native';
 
-
+interface RouteParams {
+  userDisplayName: string | undefined;
+  userPhotoURL: string | undefined;
+}
 
 const ChatScreen: React.FC = () => {
   const [message, setMessage] = useState<string>('');
-  const [messages, setMessages] = useState<any[]>([]); // Adjust the type accordingly
+  const [messages, setMessages] = useState<any[]>([]); 
   const messagesCollection = collection(db, 'messages');
+  const route = useRoute();
+  const { userDisplayName, userPhotoURL } = route.params as RouteParams;
+  const flatListRef = useRef<FlatList | null>(null);
+
+  useEffect(() => {
+    const q = query(messagesCollection, orderBy('timestamp'));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const newMessages: any[] = [];
+      querySnapshot.forEach((doc) => {
+        newMessages.push({ id: doc.id, ...doc.data() });
+      });
+      setMessages(newMessages);
+    });
+
+    return () => {
+      unsubscribe(); // Unsubscribe from real-time updates when the component unmounts
+    };
+  }, []);
+
 
   async function handleSend() {
 
@@ -20,6 +45,7 @@ const ChatScreen: React.FC = () => {
     const newMessage = {
       text: message,
       timestamp: serverTimestamp(),
+      userID: userDisplayName
     };
 
     setMessage('');
@@ -35,16 +61,40 @@ const ChatScreen: React.FC = () => {
    console.log(message);
   }
 
+  useEffect(() => {
+    // Scroll to the bottom when the component mounts and when a new message arrives
+    if (flatListRef.current) {
+      flatListRef.current.scrollToEnd({ animated: true });
+    }
+  }, [messages]);
+
   return (
     <View style={styles.container}>
       <FlatList
+        ref={(ref) => (flatListRef.current = ref)}
         data={messages}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <View style={styles.message}>
-            <Text>{item.text}</Text>
+            {userPhotoURL && (
+              <Image source={{ uri: userPhotoURL }} style={styles.userImage} />
+            )}
+            <View style={styles.messageContent}>
+              <Text style={styles.senderName}>
+                {userDisplayName}:
+              </Text>
+              <Text style={styles.messageText}>
+                {item.text}
+              </Text>
+            </View>
           </View>
         )}
+        onContentSizeChange={() => {
+          // Automatically scroll to the bottom when content size changes (new message)
+          if (flatListRef.current) {
+            flatListRef.current.scrollToEnd({ animated: true });
+          }
+        }}
       />
       <View style={styles.inputContainer}>
         <TextInput
@@ -63,18 +113,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    backgroundColor: '#f0f0f0',
   },
   message: {
+    flexDirection: 'row',
+    alignItems: 'center',
     backgroundColor: '#e6e6e6',
     padding: 8,
-    margin: 8,
+    marginVertical: 8,
+    marginHorizontal: 16,
     borderRadius: 8,
+  },
+  messageContent: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  senderName: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  messageText: {
+    fontSize: 16,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     padding: 8,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    marginTop: 16,
   },
   input: {
     flex: 1,
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,29 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
-import auth from '@react-native-firebase/auth';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image } from 'react-native';
+import auth, { FirebaseUser } from '@react-native-firebase/auth';
 import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
 
 GoogleSignin.configure({
-  webClientId: '576966639179-laprvni5190t516hgub7dpcj3u0fpaho.apps.googleusercontent.com'
+  webClientId: '576966639179-laprvni5190t516hgub7dpcj3u0fpaho.apps.googleusercontent.com',
 });
 
-const chatRooms = [
+interface ChatRoom {
+  id: string;
+  name: string;
+}
+
+interface HomeScreenProps {
+  navigation: any; // Replace with your specific navigation prop type
+}
+
+const chatRooms: ChatRoom[] = [
   { id: '1', name: 'Science' },
   { id: '2', name: 'Fitness' },
   { id: '3', name: 'Film' },
   { id: '4', name: 'Technology' },
 ];
 
-const HomeScreen: React.FC = ({ navigation }) => {
-  const [user, setUser] = useState<any | null>(null);
+const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
+  const [user, setUser] = useState<FirebaseUser | null>(null);
 
-  function onAuthStateChanged(user: any | null) {
+  function onAuthStateChanged(user: FirebaseUser | null) {
     setUser(user);
   }
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
+    return () => subscriber(); // Unsubscribe on unmount
   }, []);
 
   const signOut = async () => {
@@ -39,19 +48,21 @@ const HomeScreen: React.FC = ({ navigation }) => {
   return (
     <View style={styles.container}>
       {user ? (
-        <View>
-          <Text>Welcome, {user.displayName}</Text>
-          <TouchableOpacity
-            style={styles.signOutButton}
-            onPress={signOut}
-          >
-            <Text style={styles.buttonText}>Sign Out</Text>
-          </TouchableOpacity>
+        <View style={styles.userContainer}>
+          {user.photoURL && (
+            <Image source={{ uri: user.photoURL }} style={styles.userImage} />
+          )}
+          <View style={styles.userInfo}>
+            <Text>Welcome, {user.displayName}</Text>
+            <TouchableOpacity style={styles.signOutButton} onPress={signOut}>
+              <Text style={styles.buttonText}>Sign Out</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       ) : (
         <Text>Not signed in</Text>
       )}
-      <Text style={styles.heading}>Chat Rooms:</Text>
+      <Text style={styles.heading}>Welcome {user?.displayName}. Chat Rooms:</Text>
       <FlatList
         data={chatRooms}
         keyExtractor={(item) => item.id}
@@ -60,7 +71,10 @@ const HomeScreen: React.FC = ({ navigation }) => {
           <TouchableOpacity
             style={styles.chatRoomButton}
             onPress={() => {
-                navigation.navigate('Chat')
+              navigation.navigate('Chat', {
+                userDisplayName: user?.displayName,
+                userPhotoURL: user?.photoURL,
+              });
             }}
           >
             <Text style={styles.buttonText}>{item.name}</Text>
@@ -76,28 +90,44 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f0f0f0',
   },
   heading: {
     fontSize: 18,
-    marginVertical: 10,
   },
   chatRoomList: {
     justifyContent: 'center',
-  flexGrow: 1, // Make the chat rooms fill the screen vertically
+    flexGrow: 1,
   },
   chatRoomButton: {
     padding: 15,
     marginVertical: 10,
-    backgroundColor: 'grey', // Change the color to grey
+    backgroundColor: 'grey',
     borderRadius: 5,
+    width: '100%',
   },
   buttonText: {
     color: 'white',
+    textAlign: 'center',
+  },
+  userContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  userInfo: {
+    marginLeft: 20,
   },
   signOutButton: {
     padding: 10,
     backgroundColor: 'red',
     borderRadius: 5,
+    marginTop: 10,
   },
 });
 
